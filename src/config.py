a4035fbb6c6b0cbf5bc03a2260aeec9eca20d894
qu1nqqy <qu1nqqy@yandex.ru>
2025-03-20 21:42:34 +0300

    chore: turn alebmic into async migrations

--- src/config.py
+++ src/config.py
@@ -3,7 +3,6 @@ import os
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-    ALEMBIC_DATABASE_URL: str = os.getenv("ALEMBIC_DATABASE_URL")
     REDIS_URL: str = os.getenv("REDIS_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
