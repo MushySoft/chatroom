ece0c90078a720624e5882c89dd6757356a6b800
qu1nqqy <qu1nqqy@yandex.ru>
2025-03-21 08:54:45 +0300

    feat: add two env variables

--- src/config.py
+++ src/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     REDIS_URL: str = os.getenv("REDIS_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET")
 
     class Config:
         env_file = ".env"
