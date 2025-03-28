0886e411c68c3e0316ed710a747dcbf2825d63c2
qu1nqqy <qu1nqqy@yandex.ru>
2025-03-28 18:55:53 +0300

    feat: add deps file

--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import declarative_base
-from typing_extensions import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from src.config import settings
 
@@ -8,9 +7,12 @@ Base = declarative_base()
 
 engine = create_async_engine(settings.DATABASE_URL, future=True, echo=True)
 
-AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
 
 
-async def get_db() -> AsyncGenerator[AsyncSession]:
-    async with AsyncSessionLocal() as session:
-        yield session
+AsyncSessionLocal = async_session_maker
+
