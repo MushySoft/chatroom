9266afd15352815cb737b7832859fd7631377ec0
qu1nqqy <qu1nqqy@yandex.ru>
2025-04-12 21:53:00 +0300

    feat: add funcs to init.py

--- src/message/__init__.py
+++ src/message/__init__.py
@@ -1,5 +1,20 @@
 from src.message.router import router
+from src.message.schemas import MessageCreate, MessageUpdate
+from src.message.service import (
+    send_message,
+    get_message_by_id,
+    get_messages_by_room,
+    update_message,
+    delete_message,
+)
 
 __all__ = [
     "router",
+    "MessageCreate",
+    "MessageUpdate",
+    "send_message",
+    "get_message_by_id",
+    "get_messages_by_room",
+    "update_message",
+    "delete_message",
 ]
